74cba932c172f0bb301b18cfacdb8d33422eee82
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-11 10:47:00 -0500

    created tables

--- data/migrations/20191011102209_projects.js
+++ data/migrations/20191011102209_projects.js
@@ -1,8 +1,42 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments()
+        tbl.string('name').notNullable()
+        tbl.string('description', 255).notNullable()
+        tbl.boolean('completed').defaultTo(false)
+    }).createTable('resources', tbl => {
+        tbl.increments()
+        tbl.string('name').notNullable()
+        tbl.string('description', 255).notNullable()
+        tbl.integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .notNullable()
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        tbl.unique(['name']);
+    }).createTable('tasks', tbl => {
+        tbl.increments()
+        tbl.string('description', 255).notNullable()
+        tbl.text('notes');
+        tbl.boolean('completed').defaultTo(false)
+        tbl.integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .notNullable()
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        tbl.unique(['project_id']);
+        
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks');
 };
--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const express = require('express');
 const server = express();
 
 const projectRouter = require('./api/projects/projectsRouter');
-const resourcesRouter = require('./api/resources/resourcesRouter');
 
 server.use(express.json());
 server.use('/api/projects', projectsRouter)
-server.use('/api/resources', resourcesRouter)
 
 server.get('/', (req, res) => {
     res.send('!!!!!!!!!!!!!!')
